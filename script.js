3dcec8a97200c9f0d32d4b441b60b5811ae060a5
Hoang Hy <hoanghy01122003@gmail.com>
2023-08-26 14:02:31 +0700

    feat: add phone responsive

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ window.addEventListener("scroll", (e) => {
 	const scrollTop = window.scrollY;
 	const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-	if (scrollTop == 0) {
-		if (nav.classList.contains("nav_scroll")) {
-			nav.classList.remove("nav_scroll");
-		}
-	} else {
-		nav.classList.add("nav_scroll");
-	}
-	nav.style.paddingTop = `${Math.max(4 * rem - scrollTop, 1.3 * rem)}px`;
+	// if (scrollTop == 0) {
+	// 	if (nav.classList.contains("nav_scroll")) {
+	// 		nav.classList.remove("nav_scroll");
+	// 	}
+	// } else {
+	// 	nav.classList.add("nav_scroll");
+	// }
+	// nav.style.paddingTop = `${Math.max(4 * rem - scrollTop, 1.3 * rem)}px`;
 });
